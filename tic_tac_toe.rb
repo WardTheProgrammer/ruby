6498bf5f15a6de0d8d2d12b3c99d7d6bc4cda6e0
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-03-26 23:35:18 +0100

    Allow users to input their move and display the board

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -13,12 +13,40 @@ class Game
   def initialize()
     @grid = create_grid()
     @players = [Player.new(1, "X"), Player.new(2, "O")]
-    @current_player = @players[0]
+    @current_player_index = 0
+    @current_player = @players[@current_player_index]
     puts "Starting Player: #{@current_player.name}"
     puts "Make your move by inputting a number from the grid:"
     p [1, 2, 3]
     p [4, 5, 6]
     p [7, 8, 9]
+  end
+
+  def play()
+    winner = nil
+    while winner == nil do
+      print "#{@current_player.name}'s move: "
+      move = gets.chomp.to_i
+      if move <= 3
+        row = 0
+        column = move - 1
+      elsif move <= 6
+        row = 1
+        column = move - 4
+      elsif move <= 9
+        row = 2
+        column = move - 7
+      end
+      @grid[row][column] = @current_player.marker
+      for row in @grid do
+        p row
+      end
+      case (@current_player_index)
+      when 0 then @current_player_index = 1
+      when 1 then @current_player_index = 0
+      end
+      @current_player = @players[@current_player_index]
+    end
   end
 end
 
